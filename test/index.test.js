822deb2c3b0bc2b1d972ee39d57851ba609913cf
Ayush Patel <patel.ayush08@gmail.com>
2022-11-17 15:40:42 +0900

    Created tests
    Signed-off-by: Ayush Patel <patel.ayush08@gmail.com>

---
+++ test/index.test.js
@@ -0,0 +1,84 @@
+/*
+ * Copyright (c) 2022 Ayush Patel <patel.ayush08@gmail.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+const nock = require("nock");
+// Requiring our app implementation
+const myProbotApp = require("..");
+const { Probot, ProbotOctokit } = require("probot");
+// Requiring our fixtures
+const payload = require("./fixtures/issues.opened");
+const issueCreatedBody = { body: "Thanks for opening this issue!" };
+const fs = require("fs");
+const path = require("path");
+
+const privateKey = fs.readFileSync(
+  path.join(__dirname, "fixtures/mock-cert.pem"),
+  "utf-8"
+);
+
+describe("My Probot app", () => {
+  let probot;
+
+  beforeEach(() => {
+    nock.disableNetConnect();
+    probot = new Probot({
+      appId: 123,
+      privateKey,
+      // disable request throttling and retries for testing
+      Octokit: ProbotOctokit.defaults({
+        retry: { enabled: false },
+        throttle: { enabled: false },
+      }),
+    });
+    // Load our app into probot
+    probot.load(myProbotApp);
+  });
+
+  test("creates a comment when an issue is opened", async () => {
+    const mock = nock("https://api.github.com")
+      // Test that we correctly return a test token
+      .post("/app/installations/2/access_tokens")
+      .reply(200, {
+        token: "test",
+        permissions: {
+          issues: "write",
+        },
+      })
+
+      // Test that a comment is posted
+      .post("/repos/hiimbex/testing-things/issues/1/comments", (body) => {
+        expect(body).toMatchObject(issueCreatedBody);
+        return true;
+      })
+      .reply(200);
+
+    // Receive a webhook event
+    await probot.receive({ name: "issues", payload });
+
+    expect(mock.pendingMocks()).toStrictEqual([]);
+  });
+
+  afterEach(() => {
+    nock.cleanAll();
+    nock.enableNetConnect();
+  });
+});
+
+// For more information about testing with Jest see:
+// https://facebook.github.io/jest/
+
+// For more information about testing with Nock see:
+// https://github.com/nock/nock
