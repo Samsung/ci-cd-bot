0b41927c59102ea344dbbbf4f4206352fa17d5e7
Ayush Patel <patel.ayush08@gmail.com>
2022-11-17 15:39:26 +0900

    Initialized CI/CD Bot
    Signed-off-by: Ayush Patel <patel.ayush08@gmail.com>

---
+++ index.js
@@ -0,0 +1,37 @@
+/*
+ * Copyright (c) 2022 Ayush Patel <patel.ayush08@gmail.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * This is the main entrypoint to your Probot app
+ * @param {import('probot').Probot} app
+ */
+module.exports = (app) => {
+  // Your code here
+  app.log.info("Yay, the app was loaded!");
+
+  app.on("issues.opened", async (context) => {
+    const issueComment = context.issue({
+      body: "Thanks for opening this issue!",
+    });
+    return context.octokit.issues.createComment(issueComment);
+  });
+
+  // For more information on building apps:
+  // https://probot.github.io/docs/
+
+  // To get your app running against GitHub, see:
+  // https://probot.github.io/docs/development/
+};
